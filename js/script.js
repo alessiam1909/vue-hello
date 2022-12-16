bf0fea2edf379228c6a9ab253e4a74c1406c7367
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-16 15:04:25 +0100

    esercizio con bonus

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ createApp({
     data(){
         return{
             message: "Hello Vue!!",
-            image: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            image: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         };
 
     }
